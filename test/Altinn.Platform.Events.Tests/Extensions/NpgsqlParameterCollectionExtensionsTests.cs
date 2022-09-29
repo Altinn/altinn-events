f84b8b8510cf1334aaea7eea0e1fa025da943422
Terje Holene <terje.holene@gmail.com>
2022-09-29 18:01:09 +0200

    Add tests of NpgsqlParameterCollectionExtensions

---
+++ test/Altinn.Platform.Events.Tests/Extensions/NpgsqlParameterCollectionExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+
+using Altinn.Platform.Events.Extensions;
+
+using Npgsql;
+using System;
+using Xunit;
+
+namespace Altinn.Platform.Events.Tests.Extensions;
+
+public class NpgsqlParameterCollectionExtensionsTests
+{
+    [Fact]
+    public void AddWithNullableString_ValueIsNotNull_ParameterValueMatchInput()
+    {
+        // Arrange
+        NpgsqlCommand command = new(); // Can't construct a parameter collection directly.
+
+        const string ParameterName = "parameterName";
+        const string ParameterValue = "test";
+
+        // Act
+        command.Parameters.AddWithNullableString(ParameterName, ParameterValue);
+
+        // Assert
+        Assert.Equal(ParameterValue, command.Parameters[ParameterName].Value);
+    }
+
+    [Fact]
+    public void AddWithNullableString_ValueIsNull_ParameterValueIsDBNull()
+    {
+        // Arrange
+        NpgsqlCommand command = new(); // Can't construct a parameter collection directly.
+
+        const string ParameterName = "parameterName";
+        const string? ParameterValue = null;
+
+        // Act
+        command.Parameters.AddWithNullableString(ParameterName, ParameterValue);
+
+        // Assert
+        Assert.Equal(DBNull.Value, command.Parameters[ParameterName].Value);
+    }
+}
