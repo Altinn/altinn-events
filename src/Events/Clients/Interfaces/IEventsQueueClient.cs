9f950b52c3d6e0af75d628d71a0a1d041b831c9d
Benjamin D. Brodie <benjamin@bdbrodie.com>
2022-10-10 15:46:49 +0200

    Fix comments

--- src/Events/Clients/Interfaces/IEventsQueueClient.cs
+++ src/Events/Clients/Interfaces/IEventsQueueClient.cs
@@ -9,23 +9,23 @@ namespace Altinn.Platform.Events.Clients.Interfaces
     public interface IEventsQueueClient
     {
         /// <summary>
-        /// Pushes the provided content to the queue
+        /// Pushes the provided content to the inbound queue
         /// </summary>
         /// <param name="content">The content to push to the queue in string format</param>
         /// <returns>Returns a queue receipt</returns>
         public Task<PushQueueReceipt> PushToInboundQueue(string content);
 
         /// <summary>
-        /// Pushes the provided content to the queue
+        /// Pushes the provided content to the outbound queue
         /// </summary>
         /// <param name="content">The content to push to the queue in string format</param>
         /// <returns>Returns a queue receipt</returns>
         public Task<PushQueueReceipt> PushToOutboundQueue(string content);
 
         /// <summary>
-        /// Pushes the provided content to the queue
+        /// Pushes the provided content to the validation queue
         /// </summary>
-        /// <param name="content">The content to push to the queue in string format</param>
+        /// <param name="content">The content to push to the validation queue in string format</param>
         /// <returns>Returns a queue receipt</returns>
         public Task<PushQueueReceipt> PushToValidationQueue(string content);
     }
