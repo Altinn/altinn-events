407e1fa61192dd38ed0bfea60c641b5cfaf145bf
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2022-10-21 09:39:32 +0200

    alter and drop getsubscriptionsbyconsumer (#177)

---
+++ src/Events/Migration/v0.15/01-drop-function.sql
@@ -0,0 +1,5 @@
+﻿alter function events.getsubscriptionsbyconsumer(_consumer character varying) owner to platform_events_admin;
+
+
+drop function if exists events.getsubscriptionsbyconsumer(_consumer character varying);
+	
