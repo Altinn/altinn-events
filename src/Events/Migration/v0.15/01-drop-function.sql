fc6f7f2f66103ef448bcc9216cb696fb589d5509
acn-sbuad <stephanie.buadu@avanade.com>
2022-10-21 10:21:30 +0200

    deleted migration 0.15

--- src/Events/Migration/v0.15/01-drop-function.sql
+++
@@ -1,5 +0,0 @@
-﻿alter function events.getsubscriptionsbyconsumer(_consumer character varying) owner to platform_events_admin;
-
-
-drop function if exists events.getsubscriptionsbyconsumer(_consumer character varying);
-	
