7577dda35a333d10a55e83023b1c115174e5abd0
Benjamin D. Brodie <113984758+b-brodie-digdir@users.noreply.github.com>
2022-10-28 10:07:14 +0200

    Do not return cloudEvent when saving to db (#193)
    * Do not return cloudEvent when saving to db
    * Remove unneeded migration

--- src/Events/Migration/v0.16/01-setup-functions.sql
+++ src/Events/Migration/v0.16/01-setup-functions.sql
@@ -4,18 +4,14 @@ CREATE OR REPLACE FUNCTION events.insertevent(
 	subject character varying,
 	type character varying,
 	"time" timestamptz,
-	cloudevent INOUT text)
+	cloudevent text)
     LANGUAGE 'plpgsql'
     
 AS $BODY$
-	DECLARE storedCloudEvent text;
 
   BEGIN
 	INSERT INTO events.events(id, source, subject, type, "time", cloudevent)
 	  VALUES ($1, $2, $3, $4, $5, $6);
-
-	SELECT storedCloudEvent
-	INTO cloudevent;
   END;
 
 $BODY$;
