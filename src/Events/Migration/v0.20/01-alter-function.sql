f8496bd7825c0b10325b8e32f050322f99b0fd51
Benjamin D. Brodie <benjamin@bdbrodie.com>
2022-11-11 15:01:02 +0100

    Handle duplicate event IDs, return oldest

---
+++ src/Events/Migration/v0.20/01-alter-function.sql
@@ -0,0 +1,84 @@
+CREATE OR REPLACE FUNCTION events.get(
+	_subject character varying,
+	_after character varying,
+	_from timestamp with time zone,
+	_to timestamp with time zone,
+	_type text[],
+	_source text[],
+	_size integer)
+    RETURNS TABLE(cloudevents text)
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
+
+AS $BODY$
+BEGIN
+  RETURN query
+
+SELECT events.events_app.cloudevent
+FROM events.events_app
+WHERE (_subject = '' OR events_app.subject = _subject)
+  AND (_from IS NULL OR events_app.time >= _from)
+  AND (_to IS NULL OR events_app.time <= _to)
+  AND (_type IS NULL OR events_app.type ILIKE ANY(_type) )
+  AND (_source IS NULL OR events_app.source ILIKE ANY(_source))
+  AND (_after = '' OR events_app.sequenceno >(
+    SELECT CASE count(*) WHEN 0 THEN 0
+	  ELSE
+        (SELECT sequenceno
+        FROM events.events_app
+        WHERE id = _after
+        ORDER BY sequenceno ASC
+        LIMIT 1)
+      END
+  FROM events.events_app
+  WHERE id = _after))
+  ORDER BY events_app.sequenceno
+  limit _size;
+END;
+
+$BODY$;
+
+CREATE OR REPLACE FUNCTION events.getappevents(
+	_subject character varying,
+	_after character varying,
+	_from timestamp with time zone,
+	_to timestamp with time zone,
+	_type text[],
+	_source text[],
+	_size integer)
+    RETURNS TABLE(cloudevents text)
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
+
+AS $BODY$
+BEGIN
+return query
+	SELECT events.events_app.cloudevent
+	FROM events.events_app
+	WHERE (_subject = '' OR events_app.subject = _subject)
+	AND (_from IS NULL OR events_app.time >= _from)
+	AND (_to IS NULL OR events_app.time <= _to)
+	AND (_type IS NULL OR events_app.type ILIKE ANY(_type) )
+	AND (_source IS NULL OR events_app.source ILIKE ANY(_source))
+	AND (_after = '' OR events_app.sequenceno >(
+		SELECT
+			case count(*)
+			when 0
+				then 0
+			else
+				(SELECT sequenceno
+				FROM events.events_app
+				WHERE id = _after
+				ORDER BY sequenceno ASC
+				LIMIT 1)
+			end
+		FROM events.events_app
+		WHERE id = _after))
+  ORDER BY events_app.sequenceno
+  limit _size;
+END;
+$BODY$;
