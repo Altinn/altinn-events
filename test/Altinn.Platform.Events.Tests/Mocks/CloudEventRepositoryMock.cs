50424d73cf6b7decbfbc8efdae56a14eeb3f4cd2
Benjamin D. Brodie <benjamin@bdbrodie.com>
2022-11-17 10:32:26 +0100

    Fix tests

--- test/Altinn.Platform.Events.Tests/Mocks/CloudEventRepositoryMock.cs
+++ test/Altinn.Platform.Events.Tests/Mocks/CloudEventRepositoryMock.cs
@@ -35,62 +35,164 @@ namespace Altinn.Platform.Events.Tests.Mocks
             return Task.CompletedTask;
         }
 
+        private EventsTableEntry NewTestEvent(
+            int sequenceno,
+            string id, 
+            Uri source, 
+            string type, 
+            string subject, 
+            string alternativesubject, 
+            string time,
+            object data = null)
+        {
+            var e = new CloudEvent(CloudEventsSpecVersion.V1_0);
+            e["id"] = id;
+            e["source"] = source;
+            e["type"] = type;
+            e["subject"] = subject;
+            e["alternativesubject"] = alternativesubject;
+            e["time"] = DateTimeOffset.Parse(time);
+            e.Data = data;
+
+            var ete = new EventsTableEntry()
+            {
+                SequenceNo = sequenceno,
+                Id = id,
+                Source = source,
+                Subject = subject,
+                Time = DateTime.Parse(time),
+                Type = type,
+                CloudEvent = e
+            };
+            return ete;
+        }
+
+        private List<EventsTableEntry> GetTestEvents(int eventsCollectionNumber)
+        {
+            var events = new List<EventsTableEntry>();
+            if (eventsCollectionNumber == 1)
+            {
+                events.Add(NewTestEvent(
+                    1, 
+                    "e31dbb11-2208-4dda-a549-92a0db8c7708",
+                    new Uri("https://ttd.apps.altinn.no/ttd/endring-av-navn-v2/instances/1337/6fb3f738-6800-4f29-9f3e-1c66862656cd"),
+                    "instance.deleted", 
+                    "/party/1337", 
+                    "/person/01038712345", 
+                    "2020-10-13T11:50:29Z"));
+
+                events.Add(NewTestEvent(
+                    2,
+                    "e31dbb11-2208-4dda-a549-92a0db8c7708",
+                     new Uri("https://ttd.apps.altinn.no/ttd/endring-av-navn-v2/instances/1337/6fb3f738-6800-4f29-9f3e-1c66862656cd"),
+                    "instance.deleted",
+                    "/party/1337",
+                    "/person/01038712345",
+                    "2020-10-13T12:50:29Z"));                
+            }
+
+            if (_eventsCollection == 2)
+            {
+                events.Add(NewTestEvent(
+                    1,
+                    "e31dbb11-2208-4dda-a549-92a0db8c7708",
+                    new Uri("https://nav.apps.altinn.no/nav/app/instances/1337/e31dbb11-2208-4dda-a549-92a0db8c7708"),
+                    "instance.created",
+                    "/party/1337",
+                    "/person/01038712345",
+                    "2020-06-06T11:50:29.463221Z",
+                    "data field"));
+
+                events.Add(NewTestEvent(
+                    2,
+                    "e31dbb11-2208-4dda-a549-92a0db8c8808",
+                    new Uri("https://nav.apps.altinn.no/nav/app/instances/1337/e31dbb11-2208-4dda-a549-92a0db8c8808"),
+                    "instance.created",
+                    "/party/1337",
+                    "/person/01038712345",
+                    "2020-06-06T11:50:29.463221Z",
+                    "data field"));
+
+                events.Add(NewTestEvent(
+                    3,
+                    "e31dbb11-2208-4dda-a549-92a0db8c9908",
+                    new Uri("https://nav.apps.altinn.no/nav/app/instances/12345/e31dbb11-2208-4dda-a549-92a0db8c9908"),
+                    "instance.restored",
+                    "/party/12345",
+                    "/person/01038712345",
+                    "2020-06-16T12:51:29.463221Z",
+                    "data field"));
+
+                events.Add(NewTestEvent(
+                    4,
+                    "e31dbb11-2208-4dda-a549-92a0db8c0008",
+                    new Uri("https://nav.apps.altinn.no/nav/app/instances/12345/e31dbb11-2208-4dda-a549-92a0db8c0008"),
+                    "instance.saved",
+                    "/party/12345",
+                    "/person/01038712345",
+                    "2020-06-17T08:50:29.463221Z",
+                    "data field"));
+
+                events.Add(NewTestEvent(
+                    5,
+                    "e31dbb11-2208-4dda-a549-92a0db8c2208",
+                    new Uri("https://skd.apps.altinn.no/skd/sirius/instances/54321/e31dbb11-2208-4dda-a549-92a0db8c9908"),
+                    "instance.saved",
+                    "/party/54321",
+                    "/person/01038754321",
+                    "2020-06-18T12:51:29.463221Z",
+                    "data field"));
+            }
+
+            return events;
+        }
+
         /// <inheritdoc/>
         public Task<List<CloudEvent>> GetAppEvents(string after, DateTime? from, DateTime? to, string subject, List<string> source, List<string> type, int size)
         {
-            string eventsPath = Path.Combine(GetEventsPath(), $@"{_eventsCollection}.json");
+            var tableEntries = GetTestEvents(_eventsCollection);
 
-            if (File.Exists(eventsPath))
+            // logic for filtering on source and type not implemented.
+            IEnumerable<EventsTableEntry> filter = tableEntries;
+
+            if (!string.IsNullOrEmpty(after))
             {
-                string content = File.ReadAllText(eventsPath);
-
-                // TODO: fix deserialization to work with SDK
-                List<EventsTableEntry> tableEntries = System.Text.Json.JsonSerializer.Deserialize<List<EventsTableEntry>>(content);
-
-                // logic for filtering on source and type not implemented.
-                IEnumerable<EventsTableEntry> filter = tableEntries;
-
-                if (!string.IsNullOrEmpty(after))
-                {
-                    int sequenceNo = filter.Where(te => te.Id.Equals(after)).Select(te => te.SequenceNo).FirstOrDefault();
-                    filter = filter.Where(te => te.SequenceNo > sequenceNo);
-                }
-
-                if (from.HasValue)
-                {
-                    filter = filter.Where(te => te.Time >= from);
-                }
-
-                if (to.HasValue)
-                {
-                    filter = filter.Where(te => te.Time <= to);
-                }
-
-                if (!string.IsNullOrEmpty(subject))
-                {
-                    filter = filter.Where(te => te.Subject.Equals(subject));
-                }
-
-                if (source != null && source.Count > 0)
-                {
-                    // requires more logic to match all fancy cases.
-                    filter = filter.Where(te => source.Contains(te.Source.ToString()));
-                }
-
-                if (type != null && type.Count > 0)
-                {
-                    // requires more logic to match all fancy cases.
-                    filter = filter.Where(te => type.Contains(te.Type.ToString()));
-                }
-
-                List<CloudEvent> result = filter.Select(t => t.CloudEvent)
-                    .Take(size)
-                    .ToList();
-
-                return Task.FromResult(result);
+                int sequenceNo = filter.Where(te => te.Id.Equals(after)).Select(te => te.SequenceNo).FirstOrDefault();
+                filter = filter.Where(te => te.SequenceNo > sequenceNo);
             }
 
-            return null;
+            if (from.HasValue)
+            {
+                filter = filter.Where(te => te.Time >= from);
+            }
+
+            if (to.HasValue)
+            {
+                filter = filter.Where(te => te.Time <= to);
+            }
+
+            if (!string.IsNullOrEmpty(subject))
+            {
+                filter = filter.Where(te => te.Subject.Equals(subject));
+            }
+
+            if (source != null && source.Count > 0)
+            {
+                // requires more logic to match all fancy cases.
+                filter = filter.Where(te => source.Contains(te.Source.ToString()));
+            }
+
+            if (type != null && type.Count > 0)
+            {
+                // requires more logic to match all fancy cases.
+                filter = filter.Where(te => type.Contains(te.Type.ToString()));
+            }
+
+            List<CloudEvent> result = filter.Select(t => t.CloudEvent)
+                .Take(size)
+                .ToList();
+
+            return Task.FromResult(result);
         }
 
         private static string GetEventsPath()
