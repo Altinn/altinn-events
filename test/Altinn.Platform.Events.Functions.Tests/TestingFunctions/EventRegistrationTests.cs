d56c7f1ce84b54d1d0478475d09529e88050f297
acn-sbuad <stephanie.buadu@avanade.com>
2023-01-17 17:43:28 +0100

    added tests for registration func

---
+++ test/Altinn.Platform.Events.Functions.Tests/TestingFunctions/EventRegistrationTests.cs
@@ -0,0 +1,87 @@
+﻿using Altinn.Platform.Events.Functions.Clients.Interfaces;
+
+using CloudNative.CloudEvents;
+
+using Moq;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Altinn.Platform.Events.Functions.Tests.TestingFunctions
+{
+    public class EventRegistrationTests
+    {
+        private readonly string serializedCloudEvent; 
+
+        public EventRegistrationTests()
+        {
+            serializedCloudEvent  = "{" +
+                "\"id\":\"f276d3da-9b72-492b-9fee-9cf71e2826a2\"," +
+                "\"source\":\"https://ttd.apps.at23.altinn.cloud/ttd/apps-test/instances/50012356/8f66119a-39eb-49ea-a34e-6b99ec6af319\"," +
+                "\"specversion\":\"1.0\"," +
+                "\"type\":\"app.instance.created\"," +
+                "\"extenstionatt1\":\"Stephanie er kul\"," +
+                "\"extenstionatt2\":\"2.718281828\"" +
+                "}";
+            
+        }
+
+        [Fact]
+        public async Task Run_SaveEventFails_PostInboundNotCalled()
+        {
+            // Arrange
+            Mock<IEventsClient> clientMock = new();            
+
+            clientMock.Setup(c => c.SaveCloudEvent(It.IsAny<CloudEvent>()))
+               .ThrowsAsync(new HttpRequestException("// SaveCloudEvent with id {cloudEvent.Id} failed with status code {statusCode}"));
+
+            clientMock.
+                Setup(c => c.PostInbound(It.IsAny<CloudEvent>()))
+                .Returns(Task.CompletedTask);
+
+            EventsRegistration sut = new EventsRegistration(clientMock.Object);
+
+            // Act
+            try
+            {
+                await sut.Run(serializedCloudEvent, null);
+            }
+            catch (HttpRequestException ex)
+            {
+            }
+
+            // Assert
+            clientMock.Verify(c => c.SaveCloudEvent(It.IsAny<CloudEvent>()), Times.Once);
+            clientMock.Verify(c => c.PostInbound(It.IsAny<CloudEvent>()), Times.Never);
+
+        }
+
+        [Fact]
+        public async Task Run_SaveEventSucceeds_PostInboundCalled()
+        {
+            // Arrange
+            Mock<IEventsClient> clientMock = new();
+
+            clientMock.Setup(c => c.SaveCloudEvent(It.IsAny<CloudEvent>()))
+                .Returns(Task.CompletedTask);
+
+            clientMock.
+                Setup(c => c.PostInbound(It.IsAny<CloudEvent>()))
+                .Returns(Task.CompletedTask);
+
+            EventsRegistration sut = new EventsRegistration(clientMock.Object);
+
+            // Act
+            await sut.Run(serializedCloudEvent, null);
+
+            // Assert
+            clientMock.Verify(c => c.SaveCloudEvent(It.IsAny<CloudEvent>()), Times.Once);
+            clientMock.Verify(c => c.PostInbound(It.IsAny<CloudEvent>()), Times.Once);
+        }
+    }
+}
