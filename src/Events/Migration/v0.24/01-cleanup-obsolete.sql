0bf91a6977f682d0d42afada8d44d1a386ed1bc5
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2023-02-09 12:11:47 +0100

    Update 01-cleanup-obsolete.sql

--- src/Events/Migration/v0.24/01-cleanup-obsolete.sql
+++ src/Events/Migration/v0.24/01-cleanup-obsolete.sql
@@ -1,3 +1,3 @@
-drop function events.getsubscriptions(source character varying, subject character varying, type character varying);
+drop function if exists events.getsubscriptions(source character varying, subject character varying, type character varying);
 
-drop function events.insert_subscription(sourcefilter character varying, subjectfilter character varying, typefilter character varying, consumer character varying, endpointurl character varying, createdby character varying, validated boolean);
+drop function if exists events.insert_subscription(sourcefilter character varying, subjectfilter character varying, typefilter character varying, consumer character varying, endpointurl character varying, createdby character varying, validated boolean);
