a869ce35dfa1e079a98aa48e5d95c3a46df19741
acn-sbuad <stephanie.buadu@avanade.com>
2023-06-12 11:49:19 +0200

    webhook receiver endpoint for testing

---
+++ src/Events/Controllers/WebhookReceiverController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+using CloudNative.CloudEvents;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.Platform.Events.Controllers
+{
+    /// <summary>
+    /// Controller solely for testing purposes. 
+    /// Used as webhook endpoint for testing push of events
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("events/api/v1/webhookreceiver")]
+    public class WebhookReceiverController : ControllerBase
+    {
+        /// <summary>
+        /// Accepts an http post request and responds OK if request body can be deserialized into a cloud event.
+        /// </summary>
+        [HttpPost]
+        public async Task<ActionResult<string>> Post([FromBody] CloudEvent cloudEvent)
+        {
+            return Ok();
+        }
+    }
+}
