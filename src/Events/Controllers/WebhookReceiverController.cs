8a49a297b5c3d1a334589015307b338b6217810a
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2023-06-12 15:29:44 +0200

    webhook receiver endpoint for testing (#362)

---
+++ src/Events/Controllers/WebhookReceiverController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using CloudNative.CloudEvents;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Altinn.Platform.Events.Controllers
+{
+    /// <summary>
+    /// Controller for supporting automated tests.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    [Route("events/api/v1/tests/webhookreceiver")]
+    [Consumes("application/cloudevents+json")]
+
+    [SwaggerTag("Private API")]
+    public class WebhookReceiverController : ControllerBase
+    {
+        /// <summary>
+        /// Accepts an http post request and responds OK if request body can be deserialized into a cloud event.
+        /// </summary>
+        [HttpPost]
+        public ActionResult Post([FromBody] CloudEvent cloudEvent)
+        {
+            return Ok();
+        }
+    }
+}
