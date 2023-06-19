96e77870cfeb114e5e94b129be176007e23b1f84
Bjørn Dybvik Langfors <bjorn@langfors.no>
2023-06-19 16:51:28 +0200

    Increase query performance on GET (#368)
    * Add indices, adjust function for improved query performance on generic events
    
    * Added more indexes and changed events.get*events
    
    * Removed _resource (inserted by incurie)
    
    ---------
    
    Co-authored-by: Henning Normann <h.normann@accenture.com>

---
+++ src/Events/Migration/v0.32/01-new-indexes.sql
@@ -0,0 +1,16 @@
+--- Create B-tree indices on json columns
+CREATE INDEX IF NOT EXISTS idx_events_cloudevent_id
+    ON events.events ((cloudevent ->> 'id'));
+CREATE INDEX IF NOT EXISTS idx_events_cloudevent_subject
+    ON events.events ((cloudevent ->> 'subject'));
+CREATE INDEX IF NOT EXISTS idx_events_cloudevent_alternativesubject
+    ON events.events ((cloudevent ->> 'alternativesubject'));
+CREATE INDEX IF NOT EXISTS idx_events_cloudevent_source
+    ON events.events ((cloudevent ->> 'source'));
+CREATE INDEX IF NOT EXISTS idx_events_cloudevent_type
+    ON events.events ((cloudevent ->> 'type'));
+CREATE INDEX IF NOT EXISTS idx_events_cloudevent_time
+    ON events.events ((cloudevent ->> 'time'));
+
+-- Drop redundant GIN index
+DROP INDEX IF EXISTS events.idx_gin_events_computed_cloudevent;
