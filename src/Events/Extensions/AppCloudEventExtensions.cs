b9181a03ffa0cd1bd65fe925b97a3e54a710e532
acn-sbuad <stephanie.buadu@avanade.com>
2023-06-20 14:40:30 +0200

    define resource and instance for app events

--- src/Events/Extensions/AppCloudEventExtensions.cs
+++ src/Events/Extensions/AppCloudEventExtensions.cs
@@ -11,6 +11,8 @@ namespace Altinn.Platform.Events.Extensions
     /// </summary>
     public static class AppCloudEventExtensions
     {
+        private static string _appResourcePrefix = "urn:altinn:resource:altinnapp.";  
+
         /// <summary>
         /// Create a Cloud Event based of the AppCloudEventRequestModel
         /// </summary>
@@ -26,12 +28,44 @@ namespace Altinn.Platform.Events.Extensions
                 Type = appEvent.Type
             };
 
+            var sourceProperties = GetPropertiesFromAppSource(appEvent.Source);
+
+            cloudEvent.SetAttributeFromString("resource", $"{_appResourcePrefix}{sourceProperties.Org}.{sourceProperties.App}");
+            cloudEvent.SetAttributeFromString("resourceinstance", $"{sourceProperties.InstanceOwnerPartyId}/{sourceProperties.InstanceGuid}");
+
             if (!string.IsNullOrEmpty(appEvent.AlternativeSubject))
             {
                 cloudEvent.SetAttributeFromString("alternativesubject", appEvent.AlternativeSubject);
             }
 
             return cloudEvent;
+        }
+
+        /// <summary>
+        /// Decomposes the source of a cloud event into known parts.
+        /// </summary>
+        /// <param name="source">The uri source of the cloud event</param>
+        /// <returns>A tuple containing org, app, intanceOwnerPartyId and instanceGuid</returns>
+        /// <remarks>Domain should always be validated as a match to current environment before using this method.</remarks>
+        public static (string Org, string App, string InstanceOwnerPartyId, string InstanceGuid) GetPropertiesFromAppSource(Uri source)
+        {
+            string org = null;
+            string app = null;
+            string instanceOwnerPartyId = null;
+            string instanceGuid = null;
+
+            string[] pathParams = source.AbsolutePath.Split("/");
+
+            if (pathParams.Length > 5)
+            {
+                org = pathParams[1];
+                app = pathParams[2];
+                instanceOwnerPartyId = pathParams[4];
+                instanceGuid = pathParams[5];
+            }
+
+            return (org, app, instanceOwnerPartyId, instanceGuid);
+
         }
     }
 }
