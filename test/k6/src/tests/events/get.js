b651208462f5739b7210b0002db3d30d19414957
acn-sbuad <stephanie.buadu@avanade.com>
2023-09-01 14:43:27 +0200

    updated k6 tests

--- test/k6/src/tests/events/get.js
+++ test/k6/src/tests/events/get.js
@@ -19,95 +19,97 @@ import { addErrorCount, stopIterationOnFail } from "../../errorhandler.js";
 const scopes = "altinn:events.subscribe";
 
 export const options = {
-  thresholds: {
-    errors: ["count<1"],
-  },
+    thresholds: {
+        errors: ["count<1"],
+    },
 };
 
 export function setup() {
-  var token = setupToken.getAltinnTokenForOrg(scopes);
+    var token = setupToken.getAltinnTokenForOrg(scopes);
 
-  var cloudEvent = eventJson;
-  cloudEvent.id = uuidv4();
+    var cloudEvent = eventJson;
+    cloudEvent.id = uuidv4();
 
-  const runFullTestSet = __ENV.runFullTestSet
-    ? __ENV.runFullTestSet.toLowerCase().includes("true")
-    : false;
+    const runFullTestSet = __ENV.runFullTestSet
+        ? __ENV.runFullTestSet.toLowerCase().includes("true")
+        : false;
 
-  var data = {
-    runFullTestSet: runFullTestSet,
-    token: token,
-    cloudEvent: cloudEvent,
-  };
+    var data = {
+        runFullTestSet: runFullTestSet,
+        token: token,
+        cloudEvent: cloudEvent,
+    };
 
-  return data;
+    return data;
 }
 
 // 01 - GET the first 10 cloud events published
 function TC01_GetAllEvents(data) {
-  var response, success;
+    var response, success;
 
-  // we assume that /events/post.js has run at least once, publishing several events
+    // we assume that /events/post.js has run at least once, publishing several events
 
-  response = eventsApi.getCloudEvents(
-    {
-      after: 0,
-      source: data.cloudEvent.source,
-      type: data.cloudEvent.type,
-      subject: data.cloudEvent.subject,
-      size: 10,
-    },
-    data.token
-  );
+    response = eventsApi.getCloudEvents(
+        {
+            after: 0,
+            resource: data.cloudEvent.resource,
+            source: data.cloudEvent.source,
+            type: data.cloudEvent.type,
+            subject: data.cloudEvent.subject,
+            size: 10,
+        },
+        data.token
+    );
 
-  success = check(response, {
-    "GET all cloud events: status is 200": (r) => r.status === 200,
-  });
-  addErrorCount(success);
+    success = check(response, {
+        "GET all cloud events: status is 200": (r) => r.status === 200,
+    });
+    addErrorCount(success);
 
-  if (!success) {
-    // only continue to parse and check content if success response code
-    stopIterationOnFail(success);
-  }
+    if (!success) {
+        // only continue to parse and check content if success response code
+        stopIterationOnFail(success);
+    }
 
-  success = check(response, {
-    "GET all cloud events: at least 1 cloud event returned": (r) => {
-      var responseBody = JSON.parse(r.body);
-      return Array.isArray(responseBody) && responseBody.length >= 1;
-    },
-  });
+    success = check(response, {
+        "GET all cloud events: at least 1 cloud event returned": (r) => {
+            var responseBody = JSON.parse(r.body);
+            return Array.isArray(responseBody) && responseBody.length >= 1;
+        },
+    });
 
-  addErrorCount(success);
+    addErrorCount(success);
 }
 
 // 02 - GET events and follow next link
 function TC02_GetEventsAndFollowNextLink(data) {
-  var response, success;
+    var response, success;
 
-  response = eventsApi.getCloudEvents(
-    {
-      after: 0,
-      source: data.cloudEvent.source,
-      type: data.cloudEvent.type,
-      size: 1,
-    },
-    data.token
-  );
+    response = eventsApi.getCloudEvents(
+        {
+            after: 0,
+            source: data.cloudEvent.source,
+            resource: data.cloudEvent.resource,
+            type: data.cloudEvent.type,
+            size: 1,
+        },
+        data.token
+    );
 
-  var nextUrl = response.headers["Next"];
+    var nextUrl = response.headers["Next"];
 
-  success = check(response, {
-    "GET cloud events: status is 200": (r) => r.status === 200,
-    "GET cloud events: next link is provided ": (r) => nextUrl,
-  });
-  addErrorCount(success);
+    success = check(response, {
+        "GET cloud events: status is 200": (r) => r.status === 200,
+        "GET cloud events: next link is provided ": (r) => nextUrl,
+    });
+    addErrorCount(success);
 
-  response = eventsApi.getEventsFromNextLink(nextUrl, data.token);
-  success = check(response, {
-    "GET cloud events from next link: status is 200": (r) => r.status === 200,
-  });
+    response = eventsApi.getEventsFromNextLink(nextUrl, data.token);
+    success = check(response, {
+        "GET cloud events from next link: status is 200": (r) => r.status === 200,
+    });
 
-  addErrorCount(success);
+    addErrorCount(success);
 }
 
 /*
@@ -115,16 +117,16 @@ function TC02_GetEventsAndFollowNextLink(data) {
  * 02 - GET events and follow next link
  */
 export default function (data) {
-  try {
-    if (data.runFullTestSet) {
-      TC01_GetAllEvents(data);
-      TC02_GetEventsAndFollowNextLink(data);
-    } else {
-      // Limited test set for use case tests
-      TC01_GetAllEvents(data);
+    try {
+        if (data.runFullTestSet) {
+            TC01_GetAllEvents(data);
+            TC02_GetEventsAndFollowNextLink(data);
+        } else {
+            // Limited test set for use case tests
+            TC01_GetAllEvents(data);
+        }
+    } catch (error) {
+        addErrorCount(false);
+        throw error;
     }
-  } catch (error) {
-    addErrorCount(false);
-    throw error;
-  }
 }
