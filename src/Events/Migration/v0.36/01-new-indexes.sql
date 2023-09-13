0a7f794e07830ab8b5bc1093aca4fa0b1af9a725
Terje Holene <terje.holene@gmail.com>
2023-09-13 16:26:58 +0200

    Include sequence number in index

--- src/Events/Migration/v0.36/01-new-indexes.sql
+++ src/Events/Migration/v0.36/01-new-indexes.sql
@@ -1,2 +1,2 @@
-CREATE INDEX IF NOT EXISTS idx_events_cloudevent_resource
-    ON events.events ((cloudevent ->> 'resource'));
+CREATE INDEX IF NOT EXISTS idx_events_cloudevent_resource_sequenceno
+ON events.events ((cloudevent ->> 'resource'), sequenceno);
