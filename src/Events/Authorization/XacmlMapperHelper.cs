5bdffb0d41dac2aea78fe93312a2bf8edcf8478c
Terje Holene <terje.holene@gmail.com>
2024-02-09 14:47:38 +0100

    Support organizationnumber as accesssubject (#472)

--- src/Events/Authorization/XacmlMapperHelper.cs
+++ src/Events/Authorization/XacmlMapperHelper.cs
@@ -1,73 +1,80 @@
-﻿using System.Collections.Generic;
+﻿#nullable enable
+
 using System.Security.Claims;
 
 using Altinn.Authorization.ABAC.Xacml.JsonProfile;
 using Altinn.Common.PEP.Helpers;
 
-namespace Altinn.Platform.Events.Authorization
+namespace Altinn.Platform.Events.Authorization;
+
+/// <summary>
+/// Shared methods for mapping to xacml
+/// </summary>
+public static class XacmlMapperHelper
 {
+    private const string DefaultIssuer = "Altinn";
+
+    private const string UserPrefix = "/user/";
+    private const string OrgPrefix = "/org/";
+    private const string PartyPrefix = "/party/";
+    private const string OrganizationPrefix = "/organization/";
+
+    private const string ClaimUserId = "urn:altinn:userid";
+    private const string ClaimOrg = "urn:altinn:org";
+    private const string ClaimPartyID = "urn:altinn:partyid";
+    private const string ClaimOrganizationNumber = "urn:altinn:organizationnumber";
+
     /// <summary>
-    /// Shared methods for mapping to xacml
+    /// Generates subject attribute list 
     /// </summary>
-    public static class XacmlMapperHelper
+    /// <returns>The XacmlJsonCategory or the subject</returns>
+    public static XacmlJsonCategory CreateSubjectAttributes(string subject)
     {
-        private const string DefaultIssuer = "Altinn";
-
-        private const string UserPrefix = "/user/";
-        private const string OrgPrefix = "/org/";
-        private const string PartyPrefix = "/party/";
-
-        private const string ClaimUserId = "urn:altinn:userid";
-        private const string ClaimPartyID = "urn:altinn:partyid";
-        private const string ClaimOrg = "urn:altinn:org";
-
-        /// <summary>
-        /// Generates subject attribute list 
-        /// </summary>
-        /// <returns>The XacmlJsonCategory or the subject</returns>
-        public static XacmlJsonCategory CreateSubjectAttributes(string subject)
+        XacmlJsonCategory category = new()
         {
-            XacmlJsonCategory category = new()
-            {
-                Attribute = new List<XacmlJsonAttribute>()
-            };
+            Attribute = []
+        };
 
-            if (subject.StartsWith(UserPrefix))
-            {
-                string value = subject.Replace(UserPrefix, string.Empty);
-                category.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(ClaimUserId, value, ClaimValueTypes.String, DefaultIssuer));
-            }
-            else if (subject.StartsWith(OrgPrefix))
-            {
-                string value = subject.Replace(OrgPrefix, string.Empty);
-                category.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(ClaimOrg, value, ClaimValueTypes.String, DefaultIssuer));
-            }
-            else if (subject.StartsWith(PartyPrefix))
-            {
-                string value = subject.Replace(PartyPrefix, string.Empty);
-                category.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(ClaimPartyID, value, ClaimValueTypes.Integer, DefaultIssuer));
-            }
-
-            return category;
+        if (subject.StartsWith(UserPrefix))
+        {
+            string value = subject.Replace(UserPrefix, string.Empty);
+            category.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(ClaimUserId, value, ClaimValueTypes.String, DefaultIssuer));
+        }
+        else if (subject.StartsWith(OrgPrefix))
+        {
+            string value = subject.Replace(OrgPrefix, string.Empty);
+            category.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(ClaimOrg, value, ClaimValueTypes.String, DefaultIssuer));
+        }
+        else if (subject.StartsWith(PartyPrefix))
+        {
+            string value = subject.Replace(PartyPrefix, string.Empty);
+            category.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(ClaimPartyID, value, ClaimValueTypes.Integer, DefaultIssuer));
+        }
+        else if (subject.StartsWith(OrganizationPrefix))
+        {
+            string value = subject.Replace(OrganizationPrefix, string.Empty);
+            category.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(ClaimOrganizationNumber, value, ClaimValueTypes.String, DefaultIssuer));
         }
 
-        /// <summary>
-        /// Splits the resource attribute at the final ':'
-        /// </summary>
-        /// <param name="resource">The resource string</param>
-        /// <returns>A tuple conaining the attribute id and attribute value</returns>
-        /// <remarks>
-        /// First entry should be used as the attribute id in the xacml request
-        /// Second entry should be used as the atttribute value in the xaml request
-        /// For an Altinn App resource second entry is on format altinnapp.{org}.{app}
-        /// </remarks>
-        public static (string AttributeId, string AttributeValue) SplitResourceInTwoParts(string resource)
-        {
-            int index = resource.LastIndexOf(':');
-            string id = resource.Substring(0, index);
-            string value = resource.Substring(index + 1);
+        return category;
+    }
 
-            return (id, value);
-        }
+    /// <summary>
+    /// Splits the resource attribute at the final ':'
+    /// </summary>
+    /// <param name="resource">The resource string</param>
+    /// <returns>A tuple conaining the attribute id and attribute value</returns>
+    /// <remarks>
+    /// First entry should be used as the attribute id in the xacml request
+    /// Second entry should be used as the atttribute value in the xaml request
+    /// For an Altinn App resource second entry is on format altinnapp.{org}.{app}
+    /// </remarks>
+    public static (string AttributeId, string AttributeValue) SplitResourceInTwoParts(string resource)
+    {
+        int index = resource.LastIndexOf(':');
+        string id = resource.Substring(0, index);
+        string value = resource.Substring(index + 1);
+
+        return (id, value);
     }
 }
---
+++ test/Altinn.Platform.Events.Tests/TestingUtils/XacmlMapperHelperTests.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+
+using Altinn.Authorization.ABAC.Xacml.JsonProfile;
+using Altinn.Platform.Events.Authorization;
+
+using Xunit;
+
+namespace Altinn.Platform.Events.Tests.TestingUtils;
+
+public class XacmlMapperHelperTests
+{
+    [Theory]
+    [InlineData("/user/342345", "urn:altinn:userid", "342345")]
+    [InlineData("/org/ttd", "urn:altinn:org", "ttd")]
+    [InlineData("/party/532345", "urn:altinn:partyid", "532345")]
+    [InlineData("/organization/876765454", "urn:altinn:organizationnumber", "876765454")]
+    public void CreateSubjectAttributes_Assert_correct_attribute_id_and_value(string subject, string attributId, string value)
+    {
+        // Act
+        XacmlJsonCategory actual = XacmlMapperHelper.CreateSubjectAttributes(subject);
+
+        // Assert
+        Assert.Single(actual.Attribute);
+        Assert.Equal(attributId, actual.Attribute[0].AttributeId);
+        Assert.Equal(value, actual.Attribute[0].Value);
+    }
+}
