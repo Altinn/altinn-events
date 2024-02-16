a816ec40b738fad86ca36800720621161d14e238
Khan Mohammad R. <codemascot@hotmail.com>
2024-02-16 11:49:01 +0100

    Wrote the logic in a different way to satify sonarcloud

--- src/Events.Functions/Services/CertificateResolverService.cs
+++ src/Events.Functions/Services/CertificateResolverService.cs
@@ -48,23 +48,21 @@ namespace Altinn.Platform.Events.Functions.Services
         /// <returns></returns>
         public async Task<X509Certificate2> GetCertificateAsync()
         {
-            if (_reloadTime > DateTime.UtcNow && _cachedX509Certificate != null)
+            if (DateTime.UtcNow > _reloadTime || _cachedX509Certificate == null)
             {
-                return _cachedX509Certificate;
-            }
+                string certBase64 = await _keyVaultService.GetCertificateAsync(
+                    _keyVaultSettings.KeyVaultURI,
+                    _keyVaultSettings.PlatformCertSecretId);
 
-            string certBase64 = await _keyVaultService.GetCertificateAsync(
-                _keyVaultSettings.KeyVaultURI,
-                _keyVaultSettings.PlatformCertSecretId);
-
-            lock (_lockObject)
-            {
-                _cachedX509Certificate = new X509Certificate2(
-                    Convert.FromBase64String(certBase64),
-                    (string)null,
-                    X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
-                _reloadTime = DateTime.UtcNow.AddSeconds(_certificateResolverSettings.CacheCertLifetimeInSeconds); // Set the expiry time to one hour from now
-                _logger.LogInformation("Generated new access token.");
+                lock (_lockObject)
+                {
+                    _cachedX509Certificate = new X509Certificate2(
+                        Convert.FromBase64String(certBase64),
+                        (string)null,
+                        X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
+                    _reloadTime = DateTime.UtcNow.AddSeconds(_certificateResolverSettings.CacheCertLifetimeInSeconds); // Set the expiry time to one hour from now
+                    _logger.LogInformation("Generated new access token.");
+                }
             }
 
             return _cachedX509Certificate;
