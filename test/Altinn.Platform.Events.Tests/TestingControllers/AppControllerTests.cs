928226cc7d9df71a179b817571b7a93e26c49dd6
acn-sbuad <stephanie.buadu@avanade.com>
2024-04-10 13:05:31 +0200

    fixed build error

--- test/Altinn.Platform.Events.Tests/TestingControllers/AppControllerTests.cs
+++ test/Altinn.Platform.Events.Tests/TestingControllers/AppControllerTests.cs
@@ -480,7 +480,18 @@ namespace Altinn.Platform.Events.Tests.TestingControllers
                 // Arrange
                 string requestUri = $"{BasePath}/app/ttd/endring-av-navn-v2?after=e31dbb11-2208-4dda-a549-92a0db8c7708&party=567890";
                 Mock<IEventsService> eventsService = new Mock<IEventsService>();
-                eventsService.Setup(es => es.GetAppEvents(It.IsAny<string>(), It.IsAny<DateTime?>(), It.IsAny<DateTime?>(), It.IsAny<int>(), It.IsAny<List<string>>(), It.IsAny<string>(), It.IsAny<List<string>>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>())).Throws(new Exception());
+                eventsService
+                    .Setup(es => es.GetAppEvents(
+                        It.IsAny<string>(),
+                        It.IsAny<DateTime?>(),
+                        It.IsAny<DateTime?>(),
+                        It.IsAny<int>(),
+                        It.IsAny<List<string>>(),
+                        It.IsAny<List<string>>(),
+                        It.IsAny<string>(),
+                        It.IsAny<string>(),
+                        It.IsAny<int>()))
+                    .Throws(new Exception());
                 HttpClient client = GetTestClient(eventsService.Object);
 
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", PrincipalUtil.GetOrgToken("ttd"));
@@ -815,7 +826,18 @@ namespace Altinn.Platform.Events.Tests.TestingControllers
                 // Arrange
                 string requestUri = $"{BasePath}/app/party?after=e31dbb11-2208-4dda-a549-92a0db8c7708&party=1337";
                 Mock<IEventsService> eventsService = new Mock<IEventsService>();
-                eventsService.Setup(es => es.GetAppEvents(It.IsAny<string>(), It.IsAny<DateTime?>(), It.IsAny<DateTime?>(), It.IsAny<int>(), It.IsAny<List<string>>(), It.IsAny<string>(), It.IsAny<List<string>>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>())).Throws(new Exception());
+                eventsService
+                    .Setup(es => es.GetAppEvents(
+                        It.IsAny<string>(),
+                        It.IsAny<DateTime?>(),
+                        It.IsAny<DateTime?>(),
+                        It.IsAny<int>(),
+                        It.IsAny<List<string>>(),
+                        It.IsAny<List<string>>(),
+                        It.IsAny<string>(),
+                        It.IsAny<string>(),
+                        It.IsAny<int>()))
+                    .Throws(new Exception());
                 HttpClient client = GetTestClient(eventsService.Object);
 
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", PrincipalUtil.GetToken(1337));
@@ -868,7 +890,7 @@ namespace Altinn.Platform.Events.Tests.TestingControllers
             {
                 // Arrange
                 Mock<IEventsService> serviceMock = new();
-              
+
                 string requestUri = $"{BasePath}/app/party?from=2022-07-07T11:00:53.3917";
                 HttpClient client = GetTestClient(serviceMock.Object);
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", PrincipalUtil.GetToken(1337));
