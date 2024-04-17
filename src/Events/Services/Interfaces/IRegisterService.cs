a741c603c605045f62949b779da7df6c2a6345cb
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2024-04-17 11:42:04 +0200

    removed unused services and dependencies (#517)
    * removed unused GetParty(int partyId)
    
    * removed unused ProfileService
    
    * removed storage.interface package ref

--- src/Events/Services/Interfaces/IRegisterService.cs
+++ src/Events/Services/Interfaces/IRegisterService.cs
@@ -9,13 +9,6 @@ namespace Altinn.Platform.Events.Services.Interfaces
     public interface IRegisterService
     {
         /// <summary>
-        /// Returns party information
-        /// </summary>
-        /// <param name="partyId">The partyId</param>
-        /// <returns>The party for the given partyId</returns>
-        Task<Party> GetParty(int partyId);
-
-        /// <summary>
         /// Party lookup
         /// </summary>
         /// <param name="orgNo">organisation number</param>
