2faf48049e64df4ce3404308aee880a665f9fe75
Henning Normann <h.normann@accenture.com>
2024-04-26 16:49:16 +0200

    Avoid referencing pg_cron when running locally

--- src/Events/Migration/v0.19/01-setup-cron-job.sql
+++ src/Events/Migration/v0.19/01-setup-cron-job.sql
@@ -1,1 +1,8 @@
-SELECT cron.schedule('0 3 * * *', $$DELETE FROM events.events_app WHERE time < now() - interval '90 days'$$);
+DO
+$do$
+BEGIN
+	IF EXISTS (SELECT setting FROM pg_settings WHERE name = 'azure.customer_resource_group') THEN
+		SELECT cron.schedule('0 3 * * *', $$DELETE FROM events.events_app WHERE time < now() - interval '90 days'$$);
+	END IF;
+END
+$do$
--- src/Events/Migration/v0.21/03-setup-cron-job.sql
+++ src/Events/Migration/v0.21/03-setup-cron-job.sql
@@ -1,1 +1,8 @@
-SELECT cron.schedule('0 3 * * *', $$DELETE FROM events.events WHERE registeredtime < now() - interval '90 days'$$);
+DO
+$do$
+BEGIN
+	IF EXISTS (SELECT setting FROM pg_settings WHERE name = 'azure.customer_resource_group') THEN
+		SELECT cron.schedule('0 3 * * *', $$DELETE FROM events.events WHERE registeredtime < now() - interval '90 days'$$);
+	END IF;
+END
+$do$
