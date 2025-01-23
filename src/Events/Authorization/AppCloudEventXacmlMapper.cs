99f3c7491ea4e4dc0524e9a4f966c84ba96e566e
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-01-23 14:17:59 +0100

    Refactor AppCloudEventXacmlMapper for improved readability

--- src/Events/Authorization/AppCloudEventXacmlMapper.cs
+++ src/Events/Authorization/AppCloudEventXacmlMapper.cs
@@ -17,17 +17,16 @@ namespace Altinn.Platform.Events.Authorization
     public static class AppCloudEventXacmlMapper
     {
         /// <summary>
-        /// Create XACML request for multiple 
+        /// Creates an XACML request for multiple events using the claims principal user as the subject.
         /// </summary>
-        /// <param name="user">The user</param>
-        /// <param name="events">The list of events</param>
-        /// <returns></returns>
+        /// <param name="user">The claims principal user.</param>
+        /// <param name="events">The list of events.</param>
+        /// <returns>A <see cref="XacmlJsonRequestRoot"/> object representing the XACML request.</returns>
         public static XacmlJsonRequestRoot CreateMultiDecisionReadRequest(ClaimsPrincipal user, List<CloudEvent> events)
         {
-            List<string> actionTypes = new() { "read" };
-
-            var resourceCategory = CreateMultipleResourceCategory(events);
-            return CloudEventXacmlMapper.CreateMultiDecisionRequest(user, actionTypes, resourceCategory);
+            List<string> actionTypes = ["read"];
+            var resourceCategories = CreateMultipleResourceCategory(events);
+            return CloudEventXacmlMapper.CreateMultiDecisionRequest(user, actionTypes, resourceCategories);
         }
 
         /// <summary>
@@ -37,30 +36,30 @@ namespace Altinn.Platform.Events.Authorization
         {
             XacmlJsonRequest request = new()
             {
-                AccessSubject = new List<XacmlJsonCategory>(),
-                Action = new List<XacmlJsonCategory>(),
-                Resource = new List<XacmlJsonCategory>()
+                Action = [],
+                Resource = [],
+                AccessSubject = [],
             };
 
-            (string org, string app, string instanceOwnerPartyId, string instanceGuid) = AppCloudEventExtensions.GetPropertiesFromAppSource(cloudEvent.Source);
+            (string orgId, string appId, string instanceOwnerPartyId, string instanceGuid) = AppCloudEventExtensions.GetPropertiesFromAppSource(cloudEvent.Source);
 
             request.AccessSubject.Add(new XacmlJsonCategory().AddSubjectAttribute(subject));
             request.Action.Add(CloudEventXacmlMapper.CreateActionCategory("read"));
-            request.Resource.Add(CreateEventsResourceCategory(org, app, instanceOwnerPartyId, instanceGuid));
+            request.Resource.Add(CreateEventsResourceCategory(orgId, appId, instanceOwnerPartyId, instanceGuid));
 
             XacmlJsonRequestRoot jsonRequest = new() { Request = request };
 
             return jsonRequest;
         }
 
-        private static XacmlJsonCategory CreateEventsResourceCategory(string org, string app, string instanceOwnerPartyId, string instanceGuid, bool includeResult = false)
+        private static XacmlJsonCategory CreateEventsResourceCategory(string orgId, string appId, string instanceOwnerPartyId, string instanceGuid, bool includeResult = false)
         {
             string defaultType = CloudEventXacmlMapper.DefaultType;
             string defaultIssuer = CloudEventXacmlMapper.DefaultIssuer;
 
             XacmlJsonCategory resourceCategory = new()
             {
-                Attribute = new List<XacmlJsonAttribute>()
+                Attribute = []
             };
 
             if (!string.IsNullOrWhiteSpace(instanceOwnerPartyId))
@@ -73,14 +72,14 @@ namespace Altinn.Platform.Events.Authorization
                 resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.InstanceId, instanceOwnerPartyId + "/" + instanceGuid, defaultType, defaultIssuer, includeResult));
             }
 
-            if (!string.IsNullOrWhiteSpace(org))
+            if (!string.IsNullOrWhiteSpace(orgId))
             {
-                resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.OrgId, org, defaultType, defaultIssuer));
+                resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.OrgId, orgId, defaultType, defaultIssuer));
             }
 
-            if (!string.IsNullOrWhiteSpace(app))
+            if (!string.IsNullOrWhiteSpace(appId))
             {
-                resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.AppId, app, defaultType, defaultIssuer));
+                resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.AppId, appId, defaultType, defaultIssuer));
             }
 
             resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.AppResource, "events", defaultType, defaultIssuer));
@@ -88,47 +87,52 @@ namespace Altinn.Platform.Events.Authorization
             return resourceCategory;
         }
 
+        /// <summary>
+        /// Creates multiple resource categories for the provided list of cloud events.
+        /// </summary>
+        /// <param name="events">The list of cloud events.</param>
+        /// <returns>A list of <see cref="XacmlJsonCategory"/> objects representing the resource categories.</returns>
         private static List<XacmlJsonCategory> CreateMultipleResourceCategory(List<CloudEvent> events)
         {
+            List<XacmlJsonCategory> resourcesCategories = [];
             string defaultType = CloudEventXacmlMapper.DefaultType;
             string defaultIssuer = CloudEventXacmlMapper.DefaultIssuer;
 
-            List<XacmlJsonCategory> resourcesCategories = new();
             int counter = 1;
-
             foreach (CloudEvent cloudEvent in events)
             {
-                XacmlJsonCategory resourceCategory = new() { Attribute = new List<XacmlJsonAttribute>() };
-
                 Uri source = cloudEvent.Source;
-
                 string path = source.PathAndQuery;
-
                 string[] paths = path.Split("/");
 
-                if (paths.Length == 6)
+                if (paths.Length != 6)
                 {
-                    // This is the scenario for events related to a given instance
-                    string instanceId = paths[4] + "/" + paths[5];
-                    string instanceOwnerPartyId = cloudEvent.Subject.Split("/")[2];
-                    string org = paths[1];
-                    string app = paths[2];
-                    string eventId = cloudEvent.Id;
-
-                    if (!string.IsNullOrWhiteSpace(instanceId))
-                    {
-                        resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.InstanceId, instanceId, defaultType, defaultIssuer, true));
-                    }
-
-                    resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.AppResource, "events", defaultType, defaultIssuer));
-                    resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.EventId, eventId, defaultType, defaultIssuer, true));
-                    resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.PartyId, instanceOwnerPartyId, defaultType, defaultIssuer));
-                    resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.OrgId, org, defaultType, defaultIssuer));
-                    resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.AppId, app, defaultType, defaultIssuer));
-                    resourceCategory.Id = CloudEventXacmlMapper.ResourceId + counter.ToString();
-                    resourcesCategories.Add(resourceCategory);
-                    counter++;
+                    continue;
                 }
+
+                // This is the scenario for events related to a given instance.
+                string appId = paths[2];
+                string eventId = cloudEvent.Id;
+                string organizationId = paths[1];
+                string instanceId = paths[4] + "/" + paths[5];
+                string instanceOwnerPartyId = cloudEvent.Subject.Split("/")[2];
+
+                XacmlJsonCategory resourceCategory = new() { Attribute = [] };
+
+                if (!string.IsNullOrWhiteSpace(instanceId))
+                {
+                    resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.InstanceId, instanceId, defaultType, defaultIssuer, true));
+                }
+
+                resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.AppResource, "events", defaultType, defaultIssuer));
+                resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.EventId, eventId, defaultType, defaultIssuer, true));
+                resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.PartyId, instanceOwnerPartyId, defaultType, defaultIssuer));
+                resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.OrgId, organizationId, defaultType, defaultIssuer));
+                resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.AppId, appId, defaultType, defaultIssuer));
+                resourceCategory.Id = $"{CloudEventXacmlMapper.ResourceId}{counter}";
+                resourcesCategories.Add(resourceCategory);
+
+                counter++;
             }
 
             return resourcesCategories;
