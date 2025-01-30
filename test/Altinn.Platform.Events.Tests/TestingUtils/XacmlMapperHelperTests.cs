577b72063f76c6fa81bab2f39ef4e62014772fb2
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-01-30 13:54:38 +0100

    Update test method to ensure that the AddSubjectAttribute method of the XacmlJsonCategory class correctly adds a subject attribute with the expected AttributeId and Value when the provided subject is a system user.

--- test/Altinn.Platform.Events.Tests/TestingUtils/XacmlMapperHelperTests.cs
+++ test/Altinn.Platform.Events.Tests/TestingUtils/XacmlMapperHelperTests.cs
@@ -14,6 +14,7 @@ public class XacmlMapperHelperTests
     [InlineData("/org/ttd", "urn:altinn:org", "ttd")]
     [InlineData("/party/532345", "urn:altinn:partyid", "532345")]
     [InlineData("/organisation/876765454", "urn:altinn:organization:identifier-no", "876765454")]
+    [InlineData("/systemuser/systemUserName", "urn:altinn:systemuser", "systemUserName")]
     public void CreateSubjectAttributes_Assert_correct_attribute_id_and_value(string subject, string attributId, string value)
     {
         // Act
