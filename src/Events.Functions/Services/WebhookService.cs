6ee79640c9e06d5210c1250bb137b55d96ba8354
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-02-22 15:45:40 +0100

    Add transfer object for webhook postback log entry

--- src/Events.Functions/Services/WebhookService.cs
+++ src/Events.Functions/Services/WebhookService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ using Altinn.Platform.Events.Functions.Extensions;
 using Altinn.Platform.Events.Functions.Models;
 using Altinn.Platform.Events.Functions.Models.Payloads;
 using Altinn.Platform.Events.Functions.Services.Interfaces;
-
+using CloudNative.CloudEvents;
 using Microsoft.Extensions.Logging;
 
 namespace Altinn.Platform.Events.Functions.Services
@@ -41,6 +42,7 @@ namespace Altinn.Platform.Events.Functions.Services
                 HttpResponseMessage response = await _client.PostAsync(envelope.Endpoint, httpContent);
 
                 // todo: post to storage controller with status code
+                await PostWebhookResponseForLogging(envelope.CloudEvent, response.StatusCode);
 
                 if (!response.IsSuccessStatusCode)
                 {
@@ -55,6 +57,17 @@ namespace Altinn.Platform.Events.Functions.Services
                 _logger.LogError(e, $"// Send to webhook with subscriptionId: {envelope.SubscriptionId} failed with error message {e.Message}");
                 throw;
             }
+        }
+
+        /// <summary>
+        /// Posts the provided cloud event to the logging endpoint
+        /// </summary>
+        /// <param name="cloudEvent">The cloud event associated with the logging entry <see cref="CloudEvent"/></param>
+        /// <param name="statusCode">The status code returned from the webhook endpoint</param>
+        /// <returns></returns>
+        internal async Task PostWebhookResponseForLogging(CloudEvent cloudEvent, HttpStatusCode statusCode)
+        {
+            await _client.PostAsync("/api/storage/events/logs", new StringContent(cloudEvent.Serialize(), Encoding.UTF8, "application/cloudevents+json"));
         }
 
         /// <summary>
--- src/Events/Controllers/StorageController.cs
+++ src/Events/Controllers/StorageController.cs
@@ -101,35 +101,4 @@ namespace Altinn.Platform.Events.Controllers
             requestTelemetry.Properties.Add("appevent.id", id);
         }
     }
-
-    /// <summary>
-    /// Data transfer object for posting a log event after receiving a webhook response.
-    /// </summary>
-    public record LogEntryData
-    {
-        /// <summary>
-        /// The cloud event associated with the post action <see cref="CloudEvent"/>"/>
-        /// </summary>
-        public CloudEvent CloudEvent { get; set; }
-
-        /// <summary>
-        /// The subscription id associated with the post action. <see cref="Subscription"/>"/>
-        /// </summary>
-        public int SubscriptionId { get; set; }
-
-        /// <summary>
-        /// The consumer of the event <see cref="Subscription"/>
-        /// </summary>
-        public string Consumer { get; set; }
-
-        /// <summary>
-        /// The consumers webhook endpoint <see cref="Subscription"/>
-        /// </summary>
-        public Uri Endpoint { get; set; }
-
-        /// <summary>
-        /// The staus code returned from the subscriber endpoint
-        /// </summary>
-        public int StatusCode { get; set; }
-    }
 }
