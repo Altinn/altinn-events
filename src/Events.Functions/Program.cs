e3d3ecabaad259f5860933d9621ef4e1cbfb0db5
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-09-05 10:32:31 +0200

    Update program.cs to using hostBuilder

--- src/Events.Functions/Program.cs
+++ src/Events.Functions/Program.cs
@@ -6,41 +6,49 @@ using Altinn.Platform.Events.Functions.Services;
 using Altinn.Platform.Events.Functions.Services.Interfaces;
 
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices((context, s) =>
+    {
+        var configuration = context.Configuration;
+        
+        s.AddOptions<PlatformSettings>()
+       .Configure<IConfiguration>((settings, config) =>
+       {
+           config.GetSection("Platform").Bind(settings);
+       });
+        s.AddOptions<KeyVaultSettings>()
+        .Configure<IConfiguration>((settings, configuration) =>
+        {
+            configuration.GetSection("KeyVault").Bind(settings);
+        });
+        s.AddOptions<CertificateResolverSettings>()
+         .Configure<IConfiguration>((settings, configuration) =>
+         {
+             configuration.GetSection("CertificateResolver").Bind(settings);
+         });
 
-builder.ConfigureFunctionsWebApplication();
+        s.AddApplicationInsightsTelemetryWorkerService();
+        s.ConfigureFunctionsApplicationInsights();
 
-builder.Services.AddApplicationInsightsTelemetryWorkerService();
-builder.Services.ConfigureFunctionsApplicationInsights();
+        s.AddSingleton<IKeyVaultService, KeyVaultService>();
+        s.AddSingleton<IAccessTokenGenerator, AccessTokenGenerator>();
+        s.AddSingleton<ICertificateResolverService, CertificateResolverService>();
 
-// Configure services first
-builder.Services.Configure<PlatformSettings>(builder.Configuration.GetSection("Platform"));
-builder.Services.Configure<KeyVaultSettings>(builder.Configuration.GetSection("KeyVault"));
-builder.Services.Configure<CertificateResolverSettings>(builder.Configuration.GetSection("CertificateResolver"));
-builder.Services.Configure<EventsOutboundSettings>(builder.Configuration.GetSection("EventsOutboundSettings"));
+        s.AddHttpClient<IEventsClient, EventsClient>();
+        s.AddHttpClient<IWebhookService, WebhookService>();
 
-builder.Services.AddSingleton<IAccessTokenGenerator, AccessTokenGenerator>();
-builder.Services.AddSingleton<ICertificateResolverService, CertificateResolverService>();
-builder.Services.AddSingleton<IKeyVaultService, KeyVaultService>();
-builder.Services.AddHttpClient<IEventsClient, EventsClient>();
-builder.Services.AddHttpClient<IWebhookService, WebhookService>();
+        s.AddQueueSenders(configuration);
+        s.AddTransient<IRetryBackoffService, RetryBackoffService>();
+    }).Build();
 
-builder.Services.AddQueueSenders(builder.Configuration);
-builder.Services.AddTransient<IRetryBackoffService, RetryBackoffService>();
-
-// Build the host
-var host = builder.Build();
-
-// Get logger from service provider
-var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
-var logger = loggerFactory.CreateLogger("Startup");
-
+// Log after the host is built
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
 logger.LogInformation("Function host configured successfully");
 
-// Start the host
-await host.RunAsync();
+host.Run();
