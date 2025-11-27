e3994c97be3b9233ef1651fd9e41911d2cf17613
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2025-11-27 12:48:25 +0100

    Revert to property accessor

--- test/k6/src/config.js
+++ test/k6/src/config.js
@@ -24,8 +24,7 @@ export var authCookieNames = {
 };
 
 //Get values from environment
-console.log("__ENV[ALTINN_ENV]: ", __ENV["ALTINN_ENV"]);
-const environment = __ENV["ALTINN_ENV"].toLowerCase();
+const environment = __ENV.ALTINN_ENV.toLowerCase();
 export let baseUrl = baseUrls[environment];
 export let authCookieName = authCookieNames[environment];
 
