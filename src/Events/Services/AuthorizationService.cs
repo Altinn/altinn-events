11ea392f6e17ff0f48543cbbacb18b601215c84e
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2026-01-08 15:24:22 +0100

    Fix typo

--- src/Events/Services/AuthorizationService.cs
+++ src/Events/Services/AuthorizationService.cs
@@ -30,7 +30,7 @@ namespace Altinn.Platform.Events.Services;
 public class AuthorizationService : IAuthorization
 {
     private const string _originalSubjectKey = "originalsubjectreplacedforauthorization";
-    private const string _subcribeActionType = "subscribe";
+    private const string _subscribeActionType = "subscribe";
     private readonly IPDP _pdp;
     private readonly IClaimsPrincipalProvider _claimsPrincipalProvider;
     private readonly IRegisterService _registerService;
@@ -94,7 +94,7 @@ public class AuthorizationService : IAuthorization
         }
 
         XacmlJsonRequestRoot xacmlJsonRequest = 
-            GenericCloudEventXacmlMapper.CreateMultiDecisionRequest(consumer, _subcribeActionType, cloudEvents.ToList());
+            GenericCloudEventXacmlMapper.CreateMultiDecisionRequest(consumer, _subscribeActionType, cloudEvents.ToList());
         XacmlJsonResponse response = await _pdp.GetDecisionForRequest(xacmlJsonRequest);
 
         List<CloudEvent> authorizedEvents = FilterAuthorizedRequests(cloudEvents.ToList(), consumer, response);
@@ -147,7 +147,7 @@ public class AuthorizationService : IAuthorization
             ReplaceSubject(cloudEvent, partyIdentifiersList);
         }
 
-        XacmlJsonRequestRoot xacmlJsonRequest = GenericCloudEventXacmlMapper.CreateMultiDecisionRequestForMultipleConsumers(cloudEvent, consumers, _subcribeActionType);
+        XacmlJsonRequestRoot xacmlJsonRequest = GenericCloudEventXacmlMapper.CreateMultiDecisionRequestForMultipleConsumers(cloudEvent, consumers, _subscribeActionType);
 
         RestoreSubject(cloudEvent);
 
