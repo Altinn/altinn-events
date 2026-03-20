cd9d4a744c2604a881a6fd29719506f109595037
Holene, Terje <terje.holene@digdir.no>
2026-03-20 15:55:27 +0100

    Add register klient code for main unit lookup

---
+++ src/Events/Services/LookupMainUnitRequest.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+
+using System.Text.Json.Serialization;
+
+namespace Altinn.Platform.Events.Services;
+
+/// <summary>
+/// Request model for the lookup resource for main units
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="LookupMainUnitRequest"/> class.
+/// </remarks>
+/// <param name="orgNumber">Organization Number of the organization to lookup parent units for</param>
+public class LookupMainUnitRequest(string orgNumber)
+{
+    /// <summary>
+    /// Data containing the urn of the organization with either orgNumber, partyId or PartyUuid.
+    /// </summary>
+    [JsonPropertyName("data")]
+    public string Data { get; init; } = $"urn:altinn:organization:identifier-no:{orgNumber}";
+}
