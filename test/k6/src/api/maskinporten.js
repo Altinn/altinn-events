654ebea8490919c60a94820011942a54a6f9f31c
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2026-05-05 16:00:20 +0200

    Extract jwk decoding for readability

--- test/k6/src/api/maskinporten.js
+++ test/k6/src/api/maskinporten.js
@@ -5,7 +5,7 @@ import http from "k6/http";
 import { uuidv4 } from "https://jslib.k6.io/k6-utils/1.4.0/index.js";
 import KJUR from "https://unpkg.com/jsrsasign@10.8.6/lib/jsrsasign.js";
 
-import { buildHeaderWithContentType}  from "../apiHelpers.js";
+import { buildHeaderWithContentType } from "../apiHelpers.js";
 import * as config from "../config.js";
 import { stopIterationOnFail, addErrorCount } from "../errorhandler.js";
 
@@ -14,15 +14,15 @@ const mpClientId = __ENV.mpClientId;
 const mpKid = __ENV.mpKid;
 
 export function generateAccessToken(scopes) {
-  if(!encodedJwk){
+  if (!encodedJwk) {
     stopIterationOnFail("Required environment variable Encoded JWK (encodedJWK) was not provided", false);
   }
 
-  if(!mpClientId){
+  if (!mpClientId) {
     stopIterationOnFail("Required environment variable maskinporten client id (mpClientId) was not provided", false);
   }
 
-  if(!mpKid){
+  if (!mpKid) {
     stopIterationOnFail("Required environment variable maskinporten kid (mpKid) was not provided", false);
   }
 
@@ -69,11 +69,13 @@ function createJwtGrant(scopes) {
     jti: uuidv4(),
   };
 
+  const jwk = JSON.parse(encoding.b64decode(encodedJwk, "std", "s"));
+
   const signedJWT = KJUR.jws.JWS.sign(
     "RS256",
     header,
     payload,
-    JSON.parse(encoding.b64decode(encodedJwk, "std", "s"))
+    jwk
   );
 
   return signedJWT;
