be3526e0cdbce44e57b1776414e5f8cce2d4a931
Sigrid <sigbrats@gmail.com>
2026-06-23 10:36:57 +0200

    Use mockporten in prod for user token (#1026)

--- test/k6/src/api/authentication.js
+++ test/k6/src/api/authentication.js
@@ -2,11 +2,9 @@ import { check } from "k6";
 import http from "k6/http";
 
 import {
-  buildHeaderWithBearer,
-  buildHeaderWithContentType,
-  buildHeaderWithCookie,
+  buildHeaderWithBearer
 } from "../apiHelpers.js";
-import { platformAuthentication, portalAuthentication, authCookieName } from "../config.js";
+import { platformAuthentication } from "../config.js";
 import { stopIterationOnFail, addErrorCount } from "../errorhandler.js";
 
 const userName = __ENV.userName;
@@ -24,64 +22,6 @@ export function exchangeToAltinnToken(token, test) {
   addErrorCount(success);
   stopIterationOnFail(
     "// Setup // Authentication towards Altinn 3  Failed",
-    success,
-    res
-  );
-
-  return res.body;
-}
-
-export function authenticateUser() {
-  if (!userName) {
-    stopIterationOnFail(
-      "Required environment variable username (userName) was not provided",
-      false
-    );
-  }
-
-  if (!userPassword) {
-    stopIterationOnFail(
-      "Required environment variable user password (userPassword) was not provided",
-      false
-    );
-  }
-
-  const endpoint = portalAuthentication.authenticateWithPwd;
-
-  const requestBody = {
-    UserName: userName,
-    UserPassword: userPassword,
-  };
-
-  let params = buildHeaderWithContentType("application/json");
-
-  let res = http.post(endpoint, JSON.stringify(requestBody), params);
-
-  let success = check(res, {
-    "// Setup // Authentication towards Altinn 2 Success": (r) =>
-      r.status === 200,
-  });
-  addErrorCount(success);
-  stopIterationOnFail(
-    "// Setup // Authentication towards Altinn 2 Success",
-    success,
-    res
-  );
-
-  const aspxAuthCookie = res.cookies[authCookieName][0].value;
-
-  const refreshEndpoint = platformAuthentication.refresh;
-
-  params = buildHeaderWithCookie(authCookieName, aspxAuthCookie);
-
-  res = http.get(refreshEndpoint, params);
-  success = check(res, {
-    "// Setup // Authentication towards Altinn 3 Success": (r) =>
-      r.status === 200,
-  });
-  addErrorCount(success);
-  stopIterationOnFail(
-    "// Setup // Authentication towards Altinn 3 Success",
     success,
     res
   );
